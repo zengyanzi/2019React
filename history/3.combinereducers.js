f9d2c8d6abb866315d6c321d37b2425901c58ebd
zengyanzi <zengzhen.ucf@gmail.com>
2019-06-09 20:00:46 +0800

    context

---
+++ history/3.combinereducers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './component/Counter';
+import Todo from './component/Todo';
+    //state 是状态树，可以说任意的结构
+    //action 是一个纯对象{type:'INCREASE',amount:2}{TYPE:'DECREASE',amount:1}
+ReactDOM.render(<div>
+    <Todo/>
+    <Counter/>
+</div>,document.querySelector("#root"));
+
