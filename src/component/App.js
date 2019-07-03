7924c1ca4ff64024581ae55cb0f7477a72900bc8
zengyanzi <zengzhen.ucf@gmail.com>
2019-07-03 22:14:09 +0800

    bootstrap to router

--- src/component/App.js
+++ src/component/App.js
@@ -1,5 +1,6 @@
 import {HashRouter as Router,
-    Route
+    Route,
+    Link
 } from "react-router-dom";
 import React from "react";
 import Home from './Home';
@@ -9,9 +10,30 @@ import Profile from './Profile';
 export default (
     <Router>
         <div>
-            <Route path="/home" component={Home}/>
-            <Route path="/user" component={User}/>
-            <Route path="/profile" component={Profile}/>
+            <nav className="navbar navbar-inverse">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <div className="navbar-brand">
+                            User managment system
+                        </div>
+                    </div>
+                    <ul className="nav navbar-nav">
+                       <li><Link to="/home">Home</Link></li>
+                        <li><Link to="/user">User</Link></li>
+                        <li><Link to="/profile">Profile</Link></li>
+                    </ul>
+                </div>
+            </nav>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <Route path="/home" component={Home}/>
+                        <Route path="/user" component={User}/>
+                        <Route path="/profile" component={Profile}/>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </Router>
 )
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import App from './component/App';
 /*
 function Root(){
